61d0a74c0ae7687ffa6b03c8629810aac8667681
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-02-02 20:43:40 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,34 +5,30 @@
 #include <algorithm>
 using namespace std;
 
+
 class Triangle{
-	public:
-		void triangle(){
-			cout<<"I am a triangle\n";
-		}
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
 };
 
 class Isosceles : public Triangle{
-  	public:
-  		void isosceles(){
+    public:
+    	void isosceles(){
     		cout<<"I am an isosceles triangle\n";
+    	}
+  		//Write your code here.
+  		void description()
+  		{
+  		    cout<<"In an isosceles triangle two sides are equal\n";
   		}
 };
-class Equilateral:public Isosceles
-{
-public:
-    void equilateral()
-    {
-        cout<<"I am an equilateral triangle\n";
-    }
-};
-//Write your code here.
 
 int main(){
-
-    Equilateral eqr;
-    eqr.equilateral();
-    eqr.isosceles();
-    eqr.triangle();
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
     return 0;
 }
